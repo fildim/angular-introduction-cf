1e4f4b2933dd6b7498a94f90aa59be12ba5e59d6
filippos dimitriadis <fildimitriadis1998@gmail.com>
2023-10-23 18:36:05 +0300

    list users minor additions

--- src/app/crud-demo/list-users/list-users.component.ts
+++ src/app/crud-demo/list-users/list-users.component.ts
@@ -19,9 +19,9 @@ export class ListUsersComponent implements OnInit{
   ngOnInit(): void {
     this.service.getAllUsers().subscribe(users => {
       this.users = users;
-    })
+    });
   }
-
+  
 
 
 }
