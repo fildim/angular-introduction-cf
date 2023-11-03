804888470fd4450306f14d4419ed547f40697a2d
filippos dimitriadis <fildimitriadis1998@gmail.com>
2023-11-03 10:54:37 +0200

    welcome component and app.routes intro

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { WelcomeComponent } from "./welcome/welcome.component";
+
+export const routes: Routes = [
+    { path: "home", component: WelcomeComponent }
+]
+
+
